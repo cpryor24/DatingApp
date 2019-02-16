8310cf6ee53662bda8f7d4bb324b95e403320373
Chris Pryor <chrispryor@live.com>
2019-02-16 14:20:05 -0700

    extend User class, migrations, automapper

--- DatingApp.API/Dtos/UserForDetailedDto.cs
+++ DatingApp.API/Dtos/UserForDetailedDto.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections.Generic;
+using DatingApp.API.Models;
 
-namespace DatingApp.API.Models
+namespace DatingApp.API.Dtos
 {
-    public class User
+    public class UserForDetailedDto
     {
         public int Id { get; set; }
 
@@ -15,7 +16,7 @@ namespace DatingApp.API.Models
 
         public string Gender { get; set; }
 
-        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
 
         public string KnownAs { get; set; }
 
@@ -31,6 +32,9 @@ namespace DatingApp.API.Models
 
         public string City { get; set; }
         public string Country { get; set; }
-        public ICollection<Photo> Photos { get; set; }
+
+         public string PhotoUrl { get; set; }
+
+         public ICollection<PhotosForDetailedDto> Photos { get; set; }
     }
 }
